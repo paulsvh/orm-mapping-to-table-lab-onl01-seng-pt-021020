e7faf62a7a38c013ebe5020767f5cd6306d08ed5
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-14 02:42:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,31 +2,29 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-def initialize(name, grade, id = nil)
-  @name = name
-  @grade = grade
-  @id = id
-end
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
-def self.create_table
-  sql = <<-SQL
-    create table if not exists songs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-    );
-      SQL
+  def self.create_table
+    sql = <<-SQL
+      create table if not exists songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        );
+        SQL
     DB[:conn].execute(sql)
   end
-end
 
-def self.drop_table
-  sql = <<-SQL
-    drop table ?;
-    SQL
-    DB[:conn].execute(sql, self)
+  def self.drop_table
+    sql = <<-SQL
+      drop table ?;
+      SQL
+      DB[:conn].execute(sql, self)
   end
-end
 
 
 end
