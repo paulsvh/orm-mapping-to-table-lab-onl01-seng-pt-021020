17dc078c9efbd3ff5a37e82412baaaa90e8e16c7
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-14 02:50:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,19 +34,6 @@ attr_reader :id
   end
 
 
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grades)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
-  end
-
-
 end
 
 
