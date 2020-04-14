3c53f0e377683e46a50ce427337510a7d10f17fb
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-14 02:36:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,28 @@
 class Student
+attr_accessor :name, :grade
+attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+def initialize(name, grade, id = nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
+
+def self.create_table
+  sql = <<-SQL
+    create table if not exists songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+end
+
+
   
 end
+
+
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]  
